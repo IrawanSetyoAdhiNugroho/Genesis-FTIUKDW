13b2ed7fe44937b8c39956d589d792ec32b57fc2
Agatha Linda <agatha_lindawati@yahoo.com>
2013-11-07 22:50:06 +0700

    Create aboutus.php

---
+++ aboutus.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>About us</title>
+	
+		<script type="text/javascript" src="jquery-1.10.2.js"></script>
+		<link rel="stylesheet" href="css_24_fixed.css" type="text/css"/>
+
+	<body>
+		<div id="wrap" class="container_24"> 
+			<div id="header" class="grid_24">
+  				<a href="#"><img src="DESAINPROGWEB.jpg" width="200"/></a>
+  				<div id="search" class="grid_5">
+			    	<button></button> 
+			    	<input type="text" value="search" />
+				</div>
+  			</div>
+
+  			<div id="navigation" class="grid_24">
+				<ul>
+					<li><a href="homefixed.php" class="grid_4"><strong>HOME</strong></a></li>
+					<li><a href="howtobuy.php" class="grid_4"><strong>HOW TO BUY</strong></a></li>
+					<li><a href="#" class="grid_4"><strong>ABOUT US</strong></a></li>
+					<li><a href="#" class="grid_4"><strong>TESTIMONY</strong></a></li>
+					<li><a href="#" class="grid_4"><strong>PERSONAL</strong></a></li>
+				</ul>
+			</div>
+
+			<div id="allcategory" class="grid_4">
+				
+				<dl>
+					<dt> <a href="#"> Cars </a> </dt>
+						<dd>
+							<ul>
+								<li><a href="Sport.html"> Sport</a></li>
+								<li><a href="#"> F1 </a></li>
+								<li><a href="#"> SUV</a></li>
+								<li><a href="#"> MPV</a></li>
+								<li><a href="#"> Truck</a></li>
+								<li><a href="#"> Sedan</a></li>
+							</ul>
+						</dd>
+
+					<dt> <a href="#"> Bike </a></dt>
+						<dd>
+							<ul>
+								<li><a href="#">Motorcycle</a></li>
+								
+							</ul>
+						</dd>
+
+					<dt> <a href="#"> Air Force</a></dt>
+						<dd>
+							<ul>
+								<li><a href="#">Helicopter</a></li>
+								<li><a href="#">Jet</a></li>
+			
+							</ul>
+						</dd>
+
+					<dt> <a href="#"> Water Adventure</a></dt>
+						<dd>
+							<ul>
+								<li><a href="#">Ship</a></li>
+								<li><a href="#">Boat</a></li>
+								
+							</ul>
+						</dd>
+
+					<dt> <a href="#"> Tank</a></dt>
+						<dd>
+							<ul>
+								<li><a href="#">Leopard</a></li>
+								<li><a href="#">Tiger Fighter</a></li>
+							</ul>
+						</dd>
+				</dl>
+
+			</div>
+
+			<div id="aboutus" class="grid_14">
+				<center><h1>ABOUT US</h1></center>
+				<P align="justify">Mainan Remote Control merupakan salah satu kiprah kami dalam dunia bisnis, dengan pengalaman yang cukup lama berkecimpung dalam dunia online dan dunia RC kami siap melayani anda dengan bermodalkan kejujuran, ketekunan sepenuh hati dan ketelitian maksimal guna menggapai kepuasan anda yang merupakan orang-orang kesayangan kami.<br><br>Produk-produk yang kami jual antara lain :</P>
+				<ol>
+					<li>Cars. antara lain Sport, F1, SUV, MPV, Truck, dan sedan remote control. </li>
+					<li>Bike. yaitu segala macam motorcycle remote control</li>
+					<li>Air Force. antara lain jenis Helikopter dan jet remote control</li>
+					<li>Water Adventure. antara lain Ship dan Boat remote control</li>
+					<li>Tank. antara lain Leopard dan Tiger-fighter remote control</li>
+				</ol>
+				<p align="justify">Terima kasih atas kepercayaan anda terhadap RC-Shop.com dan kami akan membawa kepercayaan tersebut sebagai mandat utama misi kami yaitu,  "memberikan yang terbaik untuk anda” </p>
+				<h4 align="right">Admin</h4>
+			</div>
+			<div id="right" class="grid_5">
+			    
+				<div id="valid">
+					<form id='login' action='login.php' method='post' accept-charset='UTF-8' >
+						<fieldset >
+							<legend>Login</legend>
+							<input type='hidden' name='submitted' id='submitted' value='1'/>
+							<label for="username">username</label><input type="text" name="username" id='username' username="Charles_H"maxlength="50" >
+							<label for="password">password</label><input type="password" name="password" id='password' maxlength="50" password="password">
+							<input type="submit" name='Submit' value="Log In">
+							<br><br>Doesn't have account? <br><a href="#">Sign Up</a>
+						</fieldset>
+					</form>
+				</div>
+
+				<div id="cart">
+					<h3>Shopping Cart</h3>
+					<br />
+					<img src="chart.jpg">
+					<LABEL></LABEL>
+				</div>
+
+				<div id="socialmedia">
+					<center><strong>Like or Follow us:</strong>
+						<br><br>
+						<a href="https://www.facebook.com/pages/RC-SHOP/173164616214639?ref=hl"> <img src="facebook.jpg" width="35"></a><span></span>
+						<a href="https://twitter.com/CosmicRcShop"> <img src="twitter.jpg"width="35"></a>
+					</center>
+				</div>
+			</div>
+
+
+		
+			<div id="footer" class="grid_24">
+				<div id="footer-detail">
+					<center><a href="">&copy; 2013 RC-Shop.com Design by Genesis Progweb</a></center>
+				</div>
+			</div>
+
+		</div>
+	</body>
+
+</html>			
