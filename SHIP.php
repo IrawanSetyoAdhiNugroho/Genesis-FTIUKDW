35578ffbf67adb4c3c6ff4a1b4353f2214103192
tirtagraha <tgraha98@gmail.com>
2013-11-08 15:24:05 +0700

    Create SHIP.php

---
+++ SHIP.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>SHIP</title>
+	
+		<script type="text/javascript" src="jquery-1.10.2.js"></script>
+		<link rel="stylesheet" href="cssblue.css" type="text/css"/>
+		<link rel="stylesheet" type="text/css" href="engine1//style.css" media="screen" />
+		<script type="text/javascript" src="engine1//jquery.js"></script>	</head>
+
+	<body>
+		<div id="wrap" class="container_24"> 
+			<div id="header" class="grid_24">
+  				<a href="#"><img src="DESAINPROGWEB.jpg" width="200"/></a>
+  				<div id="search" class="grid_5">
+			    	<button></button> 
+			    	<input type="text" value="search" />
+				</div>
+  			</div>
+
+  			<div id="navigation" class="grid_24">
+				<ul>
+					<li><a href="homefixed.php" class="grid_4"><strong>HOME</strong></a></li>
+					<li><a href="howtobuy.php" class="grid_4"><strong>HOW TO BUY</strong></a></li>
+					<li><a href="aboutus.php" class="grid_4"><strong>ABOUT US</strong></a></li>
+					<li><a href="testimony.php" class="grid_4"><strong>TESTIMONY</strong></a></li>
+					<li><a href="#" class="grid_4"><strong>PERSONAL</strong></a></li>
+				</ul>
+			</div>
+
+			<div id="allcategory" class="grid_4">
+				
+				<dl>
+					<dt> <a href="#"> Cars </a> </dt>
+						<dd>
+							<ul>
+								<li><a href="SPORT.php"> Sport</a></li>
+								<li><a href="F1.php"> F1 </a></li>
+								<li><a href="SUV.php"> SUV</a></li>
+								<li><a href="MPV.php"> MPV</a></li>
+								<li><a href="TRUCK.php"> Truck</a></li>
+								<li><a href="SEDAN.php"> Sedan</a></li>
+							</ul>
+						</dd>
+
+					<dt> <a href="#"> Bike </a></dt>
+						<dd>
+							<ul>
+								<li><a href="MTR.php">Motorcycle</a></li>
+								
+							</ul>
+						</dd>
+
+					<dt> <a href="#"> Air Force</a></dt>
+						<dd>
+							<ul>
+								<li><a href="HELI.php">Helicopter</a></li>
+								<li><a href="JET.php">Jet</a></li>
+			
+							</ul>
+						</dd>
+
+					<dt> <a href="#"> Water Adventure</a></dt>
+						<dd>
+							<ul>
+								<li><a href="SHIP.php">Ship</a></li>
+								<li><a href="BOAT.php">Boat</a></li>
+								
+							</ul>
+						</dd>
+
+					<dt> <a href="#"> Tank</a></dt>
+						<dd>
+							<ul>
+								<li><a href="LEO.php">Leopard</a></li>
+								<li><a href="TIGER.php">Tiger Fighter</a></li>
+							</ul>
+						</dd>
+				</dl>
+
+			</div>
+
+			<div id="sport" class="grid_15">
+				<div>
+                <img id="border" src = "gambarshipprogweb/ship2.jpg" class="grid_4"/>
+            	</div>
+            	<div>
+                <img id="border" src = "gambarshipprogweb/ship1.jpg" class="grid_4"/>
+            	</div>
+            	<div>
+                <img id="border" src = "gambarshipprogweb/ship2.jpg" class="grid_4"/>
+            	</div>
+            	<div>
+                <img id="border" src = "gambarshipprogweb/ship1.jpg" class="grid_4"/>
+            	</div>
+            </div>
+
+			<div id="right" class="grid_5">
+			    
+				<div id="valid">
+					<form id='login' action='login.php' method='post' accept-charset='UTF-8' >
+						<fieldset >
+							<legend>Login</legend>
+							<input type='hidden' name='submitted' id='submitted' value='1'/>
+							<label for="username">username</label><input type="text" name="username" id='username' username="Charles_H"maxlength="50" >
+							<label for="password">password</label><input type="password" name="password" id='password' maxlength="50" password="password">
+							<input type="submit" name='Submit' value="Log In">
+							<br><br>Doesn't have account? <br><a href="#">Sign Up</a>
+						</fieldset>
+					</form>
+				</div>
+
+				<div id="cart">
+					<h3>Shopping Cart</h3>
+					<br />
+					<img src="chart.jpg">
+					<LABEL></LABEL>
+				</div>
+
+				<div id="socialmedia">
+					<center><strong>Like or Follow us:</strong>
+						<br><br>
+						<a href="https://www.facebook.com/pages/RC-SHOP/173164616214639?ref=hl"> <img src="facebook.jpg" width="35"></a><span></span>
+						<a href="https://twitter.com/CosmicRcShop"> <img src="twitter.jpg"width="35"></a>
+					</center>
+				</div>
+			</div>
+
+
+		
+			<div id="footer" class="grid_24">
+				<div id="footer-detail">
+					<center><a href="">&copy; 2013 RC-Shop.com Design by Genesis Progweb</a></center>
+				</div>
+			</div>
+
+		</div>
+	</body>
+
+</html>
+
