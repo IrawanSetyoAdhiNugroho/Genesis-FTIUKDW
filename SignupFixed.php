4646507d22a0faffdb69a75a404ed9e1e438bf9d
IrawanSetyoAdhiNugroho <irawanecek2@gmail.com>
2013-11-09 08:19:02 +0700

    Update and rename SignupFixed to SignupFixed.php

--- SignupFixed.php
+++ SignupFixed.php
@@ -16,17 +16,14 @@
 		$prop = ($_POST['prop']);
 		$corfmpass = ($_POST['corfmpass']);
 
-		$query = "INSERT INTO `genesis`.`datauser` (`email`, `username`, `password`, `nomorhp`, `nomorrekening`, `alamat`, `kodepos`, `kabupaten`, `propinsi`) VALUES ('".$mail."', '".$name."', '".$pass."', '".$hp."', '".$rekening."', '".$alamt."', '".$post."', '".$kab."', '".$prop."')";
+		$query = "INSERT INTO `genesis`.`datauser` (`email`, `username`, `password`, `nomorhp`, `nomorrekening`, `alamat`, `kodepos`, `kabupaten`, `propinsi`) VALUES ('".$mail."', '".$name."', '".$pass."', '".$hp."', '".$rekening."', '".$alamt."', '".$pos."', '".$kab."', '".$prop."')";
 		$res=mysql_query($query) or die ("Ini gagal query");
 		if ($res==1) 
 		{
-		echo "Berhasil sign up";
+			echo "Berhasil sign up";
 		}
 		else echo "Gagal Sign Up";
-		}
-		else {
-		echo "Isi semua dulu yaaa. . ";
-		}i 
+	}
 ?>
 
 
@@ -34,7 +31,7 @@
 <html>
 
 	<head>
-		<title>Sign Up</title>
+		<title>Pendaftaran</title>
 	
 		<script type="text/javascript" src="jquery-1.10.2.js"></script>
 		<link rel="stylesheet" href="css_24_fixed.css" type="text/css"/>
@@ -47,24 +44,24 @@
   				<a href="#"><img src="images/DESAINPROGWEB.jpg" width="200"/></a>
   				<div id="search" class="grid_5">
 			    	<button></button> 
-			    	<input type="text" value="search" />
+			    	<input type="text" value="pencarian" />
 				</div>
   			</div>
 
   			<div id="navigation" class="grid_24">
 				<ul>
-					<li><a href="#" class="grid_4"><strong>HOME</strong></a></li>
-					<li><a herf="howtobuy.html" class="grid_4"><strong>HOW TO BUY</strong></a></li>
-					<li><a href="#" class="grid_4"><strong>ABOUT US</strong></a></li>
-					<li><a href="#" class="grid_4"><strong>TESTIMONY</strong></a></li>
-					<li><a href="#" class="grid_4"><strong>PERSONAL</strong></a></li>
+					<li><a href="#" class="grid_4"><strong>Beranda</strong></a></li>
+					<li><a herf="howtobuy.html" class="grid_4"><strong>Cara Pembelian</strong></a></li>
+					<li><a href="#" class="grid_4"><strong>Tentang Kami</strong></a></li>
+					<li><a href="#" class="grid_4"><strong>Testimoni</strong></a></li>
+					<li><a href="#" class="grid_4"><strong>Personal</strong></a></li>
 				</ul>
 			</div>
 
 			<div id="allcategory" class="grid_4">
 				
 				<dl>
-					<dt> <a href="#"> Cars </a> </dt>
+					<dt> <a href="#"> Mobil </a> </dt>
 						<dd>
 							<ul>
 								<li><a href="Sport.html"> Sport</a></li>
@@ -76,7 +73,7 @@
 							</ul>
 						</dd>
 
-					<dt> <a href="#"> Bike </a></dt>
+					<dt> <a href="#"> Motor </a></dt>
 						<dd>
 							<ul>
 								<li><a href="#">Motorcycle</a></li>
@@ -84,7 +81,7 @@
 							</ul>
 						</dd>
 
-					<dt> <a href="#"> Air Force</a></dt>
+					<dt> <a href="#">Pesawat</a></dt>
 						<dd>
 							<ul>
 								<li><a href="#">Helicopter</a></li>
@@ -93,7 +90,7 @@
 							</ul>
 						</dd>
 
-					<dt> <a href="#"> Water Adventure</a></dt>
+					<dt> <a href="#">Kapal</a></dt>
 						<dd>
 							<ul>
 								<li><a href="#">Ship</a></li>
@@ -102,7 +99,7 @@
 							</ul>
 						</dd>
 
-					<dt> <a href="#"> Tank</a></dt>
+					<dt> <a href="#">Tank</a></dt>
 						<dd>
 							<ul>
 								<li><a href="#">Leopard</a></li>
@@ -117,27 +114,55 @@
 			<div id="right" class="grid_5">
 			    
 				<div id="valid">
-					<form id='login' action='login.php' method='post' accept-charset='UTF-8' >
-						<fieldset >
-							<legend>Login</legend>
-							<input type='hidden' name='submitted' id='submitted' value='1'/>
-							<label for="username">username</label><input type="text" name="username" id='username' username="Charles_H"maxlength="50" >
-							<label for="password">password</label><input type="password" name="password" id='password' maxlength="50" password="password">
-							<input type="submit" name='Submit' value="Log In">
-							<br><br>Doesn't have account? <br><a href="#">Sign Up</a>
-						</fieldset>
-					</form>
+					<fieldset >
+						<legend>Masuk</legend>
+						<form id='login' action='SignupFixed.php' method='post'>
+							<label for="email">Email</label>
+								<input type="text" name="email">
+							<label for="password">password</label>
+								<input type="password" name="password">
+							<input type="submit" value="Masuk" name="submit">
+						</form>
+						<?php
+							include("koneksi.php");
+							if(isset($_POST['submit']))
+							{
+								$email = $_POST['email'];
+								$password = $_POST['password'];
+								$query = "SELECT * FROM datauser WHERE email = '$email' AND password = '$password'";
+								$hasilquery = mysql_query($query);
+								$count = mysql_num_rows($hasilquery);
+				
+								while ($data = mysql_fetch_assoc($hasilquery)) {
+								$admin = $data['admin'];
+								}
+
+								if($count == 0)
+									echo "username dan password salah";
+								else
+								{
+									session_start();
+									$_SESSION['email'] = $email;
+									if($admin == 1)
+										header("location:admin.php");
+									else if($admin == 0)
+										header("location:main.php");
+								}
+							}
+						?>		
+						<br><br>Belum mendaftar ? <br><a href="#">Daftar</a>
+					</fieldset>
 				</div>
 
 				<div id="cart">
-					<h3>Shopping Cart</h3>
+					<h3>Keranjang Belanja</h3>
 					<br />
 					<img src="images/chart.jpg">
 					<LABEL></LABEL>
 				</div>
 
 				<div id="socialmedia">
-					<center><strong>Like or Follow us:</strong>
+					<center><strong>Like dan Follow kami:</strong>
 						<br><br>
 						<a href=""> <img src="images/facebook.jpg" width="35"></a><span></span>
 						<a href=""> <img src="images/twitter.jpg"width="35"></a>
@@ -145,14 +170,14 @@
 				</div>
 			</div>
 
-<!--Batas Testimony atas-->
+
 			
 		<div id="signup" class="grid_14">
-				<center><h1>Create New Account</h1></center>
+				<center><h1>Pendaftaran</h1></center>
 				<form id='signupform' action='SignupFixed.php' method='post'>
 				 	<table>
 						<tr>
-				            <td>Name</td>
+				            <td>Nama</td>
 				            <td><input type="text" name="name" /></td>
 				        </tr>
 
@@ -162,53 +187,51 @@
 				        </tr>
 
 						<tr>
-				            <td>Phone number</td>
+				            <td>Nomor Ponsel</td>
 				            <td><input type="text" name="hp" /></td>
 				        </tr>
 
 				        <tr>
-				            <td>Address</td>
+				            <td>Alamat</td>
 				            <td><input type="text" name="alamt" /></td>
 				        </tr>
 
 				        <tr>
-				            <td>Post Code</td>
+				            <td>Kode POS</td>
 				            <td><input type="text" name="pos"  /></td>
 				        </tr>
 
 				        <tr>
-				            <td>Regency</td>
+				            <td>Kabupaten</td>
 				            <td><input type="text" name="kab" /></td>
 				        </tr>
 
 				        <tr>
-				            <td>Province</td>
+				            <td>Propinsi</td>
 				            <td><input type="text" name="prop" /></td>
 				        </tr>
 
 				        <tr>
-				            <td>Bank Account:</td>
+				            <td>Nomor Rekening</td>
 				            <td><input type="text" name="rekening" /></td>
 				        </tr>
 
 						<tr>
-				            <td>Password:</td>
+				            <td>Kata Sandi</td>
 				            <td><input type="password" name="pass" /></td>
 				        </tr>
 				        
 						<tr>
-				            <td>Confirm Password:</td>
+				            <td>Ketik Ulang Kata Sandi</td>
 				            <td><input type="password" name="corfmpass" /></td>
 				        </tr>
 				        <tr><td></td></tr><tr></tr><tr></tr><tr></tr>
 						<tr>
-				            <td></td><td></td><td><input type="submit" value="Register" /></td>
+				            <td></td><td></td><td><input type="submit" value="Daftar" /></td>
 				        </tr>
 					</table>
 				</form>
 			</div>
-
-<!--Batas komentar bawah -->
 
 			<div id="footer" class="grid_24">
 				<div id="footer-detail">
