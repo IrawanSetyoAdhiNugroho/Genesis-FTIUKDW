660c9a34a7c9828786a4121688eca9444e385ff0
IrawanSetyoAdhiNugroho <irawanecek2@gmail.com>
2013-11-09 08:24:53 +0700

    Create koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php
+	$host="localhost";
+	$user="root";
+	$pass="";
+	$database="genesis";
+	mysql_connect($host,$user,$pass) or die ("Gagal konek ke database");
+
+	mysql_select_db($database) or die ("Gagal pilih database");
+?>
