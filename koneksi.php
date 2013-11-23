c5af6738c0d69765a180d12a4b440ea5231ec743
Agatha Linda <agatha_lindawati@yahoo.com>
2013-11-23 11:16:53 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
-	$host="localhost";
-	$user="root";
-	$pass="";
-	$database="genesis";
+	$host="mysql.idhostinger.com";
+	$user="u134230580_admin";
+	$pass="genesis777";
+	$database="u134230580_genes";
 	mysql_connect($host,$user,$pass) or die ("Gagal konek ke database");
 
 	mysql_select_db($database) or die ("Gagal pilih database");
